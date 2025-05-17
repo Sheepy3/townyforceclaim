2989f6a96a3ba58b41501e77f6aedea17bbf93fe
Sheepy3 <sheepy680@gmail.com>
2025-05-16 23:39:32 -0400

    initial commit

---
+++ src/main/java/towny/sheepy/townyForceclaim/commands/forceclaim.java
@@ -0,0 +1,71 @@
+package towny.sheepy.townyForceclaim.commands;
+
+import com.palmergames.bukkit.towny.exceptions.AlreadyRegisteredException;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.TownyMessaging;
+import com.palmergames.bukkit.towny.TownyUniverse;
+
+public class forceclaim implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // Expect: /townyadmin claim <town> <x> <z>
+        if (args.length != 3) {
+            TownyMessaging.sendErrorMsg(sender, "Usage: /townyadmin claim <town> <x> <z>");
+            return true;
+        }
+
+        String townName = args[0];
+        Town town = TownyUniverse.getInstance().getTown(townName);
+        if (town == null) {
+            TownyMessaging.sendErrorMsg(sender, "Town not found: " + townName);
+            return true;
+        }
+
+        TownyWorld world = TownyAPI.getInstance().getTownyWorld("world");
+
+        int coordx,coordy;
+        coordx=Integer.parseInt(args[1]);
+        coordy=Integer.parseInt(args[2]);
+        WorldCoord worldCoord = WorldCoord.parseWorldCoord(String.valueOf(world), coordx,coordy);
+        Coord coord =worldCoord.getCoord();
+        int chunkX= coord.getX();
+        int chunkZ= coord.getZ();
+
+        if (world.hasTownBlock(coord)) {
+            try {
+                TownBlock townblock = TownyUniverse.getInstance().getTownBlock(worldCoord);
+                TownyMessaging.sendErrorMsg(sender, "Chunk " + coord.toString() + " is already claimed by" + townblock.getTown().getName());
+            } catch (NotRegisteredException e) {
+                TownyMessaging.sendMsg(sender, "Chunk " + coord + " is unclaimed.");            }
+        }
+
+        // Create & register the claim
+        TownyMessaging.sendMsg(sender,
+                String.format("registering claim..."+coord.toString()));
+
+        TownBlock newBlock = new TownBlock(chunkX, chunkZ, world);
+        newBlock.setTown(town);
+        newBlock.setType(TownBlockType.RESIDENTIAL);
+
+        TownyUniverse.getInstance().addTownBlock(newBlock);
+        newBlock.save();  // Persist immediately
+
+        TownyMessaging.sendMsg(sender,
+                String.format("Chunk (%d, %d) force‑claimed for town %s.", chunkX, chunkZ, town.getName())
+        );
+
+        // Refresh protections immediately
+        Towny.getPlugin().resetCache();
+
+        return true;
+    }
+}
